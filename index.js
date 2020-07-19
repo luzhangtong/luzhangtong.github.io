32fe811bf6d061cf1cb4e7481f1ebd58de48dc76
lzt <1475104108@qq.com>
2020-07-20 01:33:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-window.onload = function(){
-   var footers=document.getElementsByTagName("footer");
-   var parents=document.getElementsByClassName("wrapper");
-   var parent=parents[0];
-   parent.removeChild(footers[0]);
+var footers=document.getElementsByTagName("footer");
+var parents=document.getElementsByClassName("wrapper");
+var parent=parents[0];
+parent.removeChild(footers[0]);
 
-   var headers=document.getElementsByTagName("header");
-   parent.removeChild(headers[0]);
+var headers=document.getElementsByTagName("header");
+parent.removeChild(headers[0]);
+window.onload = function(){
+ 
 } 
