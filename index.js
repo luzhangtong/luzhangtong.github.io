f00279bae32a2989a20aa1dd293df629b0840752
lzt <1475104108@qq.com>
2020-07-20 01:23:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-$(function(){
- alert("TEST")
-})
+window.onload = function(){
+   var footers=document.getElementsByTagName("footer");
+   var parents=document.getElementsByClassName("wrapper");
+   var parent=parents[0];
+   for (var i = 0;i < footers.length;i++){
+     parent.removeChild(footers[i]);
+   }
+} 
