eeef58c92aaf6716d3f609254edad4bdfc405607
lzt <1475104108@qq.com>
2020-07-20 13:03:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function(){
-   var footers=document.getElementsByTagName("footer");
+/*    var footers=document.getElementsByTagName("footer");
    var parents=document.getElementsByClassName("wrapper");
    var parent=parents[0];
    parent.removeChild(footers[0]);
 
    var headers=document.getElementsByTagName("header");
-   parent.removeChild(headers[0]);
+   parent.removeChild(headers[0]); */
 } 
